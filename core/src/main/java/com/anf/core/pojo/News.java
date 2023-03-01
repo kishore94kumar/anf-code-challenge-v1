0eb63a4891d5414d5b028fb2c8cdd18c480b8620
Kishore Kumar M <kishorekumar.m@royalcyber.com>
2023-03-01 14:35:00 +0530

    EXERCISE-2 - Added Lombok for pojos

--- core/src/main/java/com/anf/core/pojo/News.java
+++ core/src/main/java/com/anf/core/pojo/News.java
@@ -1,10 +1,13 @@
 package com.anf.core.pojo;
 
+import lombok.Data;
+
 /**
  * 
  * @author Kishore
  *
  */
+@Data
 public class News {
 
 	private String author;
@@ -14,48 +17,5 @@ public class News {
 	private String url;
 	private String urlImage;
 	private String date;
-
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public String getUrlImage() {
-		return urlImage;
-	}
-	public void setUrlImage(String urlImage) {
-		this.urlImage = urlImage;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
 
 }
